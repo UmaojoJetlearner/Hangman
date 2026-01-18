da7d25b4fa4ab3d2ade150ec4a31a9b1414f840e
UmaojoJetlearner <umaojo.okpanachi@gmail.com>
2026-01-18 13:51:19 -0600

    Add files via upload

---
+++ Hnagman/Loop.py
@@ -0,0 +1,14 @@
+# i=2
+
+# while i<=10:
+#     print(i)
+#     i=i+2
+    
+# s=1
+
+# while s<=7:
+#     print(s)
+#     s=s+1
+
+for i in range(1,11):
+    print(i)
--- Hnagman/Main.py
+++ Hnagman/Main.py
@@ -1,16 +1,31 @@
 import random
 print("Welcome to Hangman where you will have guess one letter at a time.")
+maximum_atempts=6
+atempts=0
 
 word_list=["Programmer","Coder","Developer","Player","List","Game"]
 random_word=random.choice(word_list)
 letters=len(random_word)*["_"]
 print("Word:"+" ".join(letters))
 
-guess=input("Guess the letters in this word ")
-if guess in random_word:
-    print("Good guess, keep going!")
+while atempts<maximum_atempts and "_" in letters:
+
+    guess=input("Guess the letters in this word ")
+    if guess in random_word:
+        print("Good guess, keep going!")
+        for i in range(len(random_word)):
+            if random_word[i]==guess:
+                letters[i]=guess
+        print("Word:"+" ".join(letters))
+
+    else:
+        atempts+=1
+        print("Incorrect guess, try harder!")
+
+if "_" not in letters:
+    print("Congrats, You guessed the word")
 
 else:
-    print("Incorrect guess, try harder!")
+    print("Game over the original word was "+ random_word)
     
 
